84f6b8375f51e8e329c2005224149fe83bb31a99
usuarioman <castilla.ar@gmail.com>
2024-01-22 20:01:19 -0600

    add tests

---
+++ model-package/tests/test_features.py
@@ -0,0 +1,16 @@
+from regression_model.config.core import config
+from regression_model.processing.features import TemporalVariableTransformer
+
+def test_temporal_variable_transformer(sample_input_data):
+    # Dado
+    transformer = TemporalVariableTransformer(
+        variables=config.model_config.temporal_vars,  # YearRemodAdd
+        reference_variable=config.model_config.ref_var,
+    )
+    assert sample_input_data["YearRemodAdd"].iat[0] == 1961
+
+    # Cuando
+    subject = transformer.fit_transform(sample_input_data)
+
+    # Entonces
+    assert subject["YearRemodAdd"].iat[0] == 49
