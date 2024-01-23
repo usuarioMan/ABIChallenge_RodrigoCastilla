9d7a82cb9d9ba4481f6ec66ffc228b1fd320f37c
usuarioman <castilla.ar@gmail.com>
2024-01-22 23:11:02 -0600

    add test for the api

---
+++ house-price-api/app/tests/conftest.py
@@ -0,0 +1,21 @@
+from typing import Generator
+
+import pandas as pd
+import pytest
+from fastapi.testclient import TestClient
+from regression_model.config.core import config
+from regression_model.processing.data_manager import load_dataset
+
+from app.main import app
+
+# Fixture para cargar datos de prueba
+@pytest.fixture(scope="module")
+def test_data() -> pd.DataFrame:
+    return load_dataset(file_name=config.app_config.test_data_file)
+
+# Fixture para inicializar el cliente de prueba
+@pytest.fixture()
+def client() -> Generator:
+    with TestClient(app) as _client:
+        yield _client
+        app.dependency_overrides = {}  # Restablecer las dependencias después de cada prueba
---
+++ house-price-api/app/tests/test_api.py
@@ -0,0 +1,34 @@
+import math
+
+import numpy as np
+import pandas as pd
+from fastapi.testclient import TestClient
+
+def test_make_prediction(client: TestClient, test_data: pd.DataFrame) -> None:
+    # Given
+    # Preparar los datos de prueba en el formato esperado por el modelo
+    payload = {
+        # Asegurarse de que Pydantic funcione bien con np.nan
+        "inputs": test_data.replace({np.nan: None}).to_dict(orient="records")
+    }
+
+    # When
+    # Enviar la solicitud de predicción al endpoint correspondiente
+    response = client.post(
+        "http://localhost:8001/api/v1/predict",
+        json=payload,
+    )
+
+    # Then
+    # Verificar que la respuesta tenga el código de estado esperado
+    assert response.status_code == 200
+
+    # Extraer los datos de predicción de la respuesta
+    prediction_data = response.json()
+
+    # Verificar que se obtuvieron predicciones y no hay errores
+    assert prediction_data["predictions"]
+    assert prediction_data["errors"] is None
+
+    # Verificar que la predicción está dentro de una tolerancia relativa específica
+    assert math.isclose(prediction_data["predictions"][0], 113422, rel_tol=100)
