3db4d9e0f6d2d051b4cce98d29228a57d2aa9ac7
usuarioman <castilla.ar@gmail.com>
2024-01-31 15:21:16 -0600

    fix typo on singleton

--- house-prices-api/database/mongo_singleton.py
+++ house-prices-api/database/mongo_singleton.py
@@ -3,7 +3,7 @@ from loguru import logger
 from decouple import config
 
 
-MONGODBURI = mongodb_uri = config('MONGODBURI')
+MONGODBURI = config('MONGODBURI')
 
 def crear_cliente_mongodb():
     try:
